f448bf6078c51e9b5c6c3d346c9230e572f835ce
HeisenbergfromIN <pranavrpalkar@gmail.com>
2018-04-18 15:40:21 +0530

    Update files

--- site.js
+++ site.js
@@ -11,6 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registration) {
+navigator.serviceWorker && navigator.serviceWorker.register('/sw.js').then(function(registration) {
     console.log('Excellent, registered with scope: ', registration.scope);
 });
